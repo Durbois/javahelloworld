d6ee56516d00a8a70bed74c80a063de516ff8056
Durbois <taghuofongue@camoo.cm>
2017-09-08 05:33:37 +0200

    commit

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,6 +3,6 @@ public class JavaHelloWorld{
  public static void main(String [] args){
      System.out.println("Java HEllo World");
      System.out.println("Hello again");
-     System.out.println("Hello my gut friends");
+     System.out.println("Hello my gut friends, how are you?");
  }
 }
